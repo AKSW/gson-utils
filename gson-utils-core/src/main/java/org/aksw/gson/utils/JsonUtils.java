acb343435e5152e1f0c88c05d79ccfa8fe82f7c5
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2015-10-03 18:57:13 +0200

    update

--- gson-utils-core/src/main/java/org/aksw/gson/utils/JsonUtils.java
+++ gson-utils-core/src/main/java/org/aksw/gson/utils/JsonUtils.java
@@ -1,7 +1,10 @@
 package org.aksw.gson.utils;
 
+import java.util.Map.Entry;
+
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 public class JsonUtils {
     public static JsonElement safeGet(JsonArray arr, int i) {
@@ -14,4 +17,22 @@ public class JsonUtils {
         Object result = JsonTransformerUtils.toJavaObject(tmp);
         return result;
     }
+
+    /**
+     * Add all entries of source to target. Does not perform a deep copy.
+     * @param target
+     * @param source
+     * @return
+     */
+    public static JsonObject extend(JsonObject target, JsonObject source) {
+        for(Entry<String, JsonElement> entry : source.entrySet()) {
+            String key = entry.getKey();
+            JsonElement val = entry.getValue();
+
+            target.add(key, val);
+        }
+
+        return target;
+    }
+
 }
